acd01a51399e7830cd9207b2aa9e55f62a436ec3
Jerrie Pelser <jerrie@jerriepelser.com>
2020-02-13 15:58:49 +0700

    Initial app

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WeightRecorder.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public DbSet<RecordedWeight> RecordedWeights { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor)
+            : base(options)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<RecordedWeight>()
+                .HasQueryFilter(weight => weight.UserId == _httpContextAccessor.HttpContext.User.GetUserId());
+        }
+    }
+}
